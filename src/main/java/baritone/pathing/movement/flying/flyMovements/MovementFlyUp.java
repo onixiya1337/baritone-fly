e87cea318062b82821deb3aab8d0e479c543345f
Nirox <kieransande@gmail.com>
2023-11-25 13:58:44 +0100

    Fly update (hopefully the last 1)

---
+++ src/main/java/baritone/pathing/movement/flying/flyMovements/MovementFlyUp.java
@@ -0,0 +1,56 @@
+package baritone.pathing.movement.flying.flyMovements;
+
+import baritone.api.IBaritone;
+import baritone.api.pathing.movement.MovementStatus;
+import baritone.api.utils.BetterBlockPos;
+import baritone.pathing.movement.CalculationContext;
+import baritone.pathing.movement.Movement;
+import baritone.pathing.movement.MovementHelper;
+import baritone.pathing.movement.MovementState;
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.block.state.IBlockState;
+
+import java.util.Set;
+
+public class MovementFlyUp extends Movement {
+
+
+    public MovementFlyUp(IBaritone baritone, BetterBlockPos start, BetterBlockPos end) {
+        super(baritone, start, end);
+    }
+
+    @Override
+    public double calculateCost(CalculationContext context) {
+        return cost(context, src.x, src.y, src.z);
+    }
+
+    @Override
+    protected Set<BetterBlockPos> calculateValidPositions() {
+        return ImmutableSet.of(src, dest);
+    }
+
+    public static double cost(CalculationContext context, int x, int y, int z) {
+        IBlockState pb0 = context.get(x, y + 2, z);
+
+        if (!MovementHelper.canWalkThrough(context, x, y + 2, z, pb0) && !MovementHelper.isWater(pb0.getBlock())) {
+            return COST_INF;
+        }
+
+        return WALK_ONE_BLOCK_COST;
+    }
+
+    @Override
+    public MovementState updateState(MovementState state) {
+        super.updateState(state);
+        if (state.getStatus() != MovementStatus.RUNNING) {
+            return state;
+        }
+
+        return state;
+    }
+
+    @Override
+    protected boolean prepared(MovementState state) {
+        return true;
+    }
+}
